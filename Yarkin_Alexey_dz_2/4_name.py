119bda0e703153c0b16d663a00412730e33683e8
Alexey <81236813+aarkin002@users.noreply.github.com>
2021-04-14 12:44:58 +0300

    Delete Yarkin_Alexey_dz_2 directory

--- Yarkin_Alexey_dz_2/4_name.py
+++
@@ -1,5 +0,0 @@
-usr_lst = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-
-for i in range(len(usr_lst)):
-    usr_lst_parts = usr_lst[i].split(' ')
-    print('Привет,', usr_lst_parts[-1].capitalize() + '!')
